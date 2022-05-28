c8e184704df5cf9afc18ce1a2d76d8ce334b601a
Klab93 <kylian.bourcoud@gmail.com>
2022-05-28 18:06:57 +0200

    save 2

--- 03_code/serverAPI/index.js
+++ 03_code/serverAPI/index.js
@@ -8,11 +8,11 @@ const {Pool} = require('pg')
 app.use(cors())
 
 const credentials = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'planHEIG',
-    password: 'admin',
-    port: '5432'
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT || '5432',
+    database: process.env.DB_NAME || 'plan',
 }
 
 const pool = new Pool(credentials);
@@ -28,49 +28,56 @@ async function getFloorGis(tableName, type) {
 
 async function getAllRoomGis() {
     const text = `SELECT json_build_object(
-    'type', 'FeatureCollection',
-    'features', json_agg(ST_AsGeoJSON(room.*)::json)
-    ) 
+        'type', 'FeatureCollection',
+        'features', json_agg(ST_AsGeoJSON(room.*)::json)
+    )
+    FROM room` ;
+    return pool.query(text);
+}
+
+async function getRoomGis(roomName) {
+    const text = `SELECT json_build_object(
+        'type', 'FeatureCollection',
+        'features', json_agg(ST_AsGeoJSON(room.*)::json)
+    )
+    FROM room WHERE room.name = '${roomName}'` ;
+    return pool.query(text);
+}
+
+async function getAllRoomName() {
+    const text = `SELECT name
     FROM room` ;
     return pool.query(text);
 }
 
 app.get('/line', async (req, res) => {
+
     const data = await getFloorGis("floor_geometry", 'line');
-    res.send(JSON.stringify(data.rows[0].json_build_object))
+    console.log(data.rows[0].json_build_object)
+    res.json(data.rows[0].json_build_object)
 })
 
 
 app.get('/polygon', async (req, res) => {
     const data = await getFloorGis("floor_geometry", 'polygon');
-    res.send(JSON.stringify(data.rows[0].json_build_object))
+    res.json(data.rows[0].json_build_object)
 })
 
 
-app.get('/rooms', async (req, res) => {
+app.get('/rooms/gis', async (req, res) => {
     const data = await getAllRoomGis();
-    res.send(JSON.stringify(data.rows[0].json_build_object))
+    res.json(data.rows[0].json_build_object)
 })
 
-/*
-app.get('/polylines_cafet', (req, res) => {
-    const raw = fs.readFileSync("E_Step/E_polylines_cafet.geojson");
-    const json = JSON.parse(raw)
-    res.send(JSON.stringify(json))
+app.get('/rooms/name', async (req, res) => {
+    const data = await getAllRoomName();
+    console.log(data)
+    res.json(data.rows)
 })
 
-app.get('/e_step_cafet', (req, res) => {
-    const raw = fs.readFileSync("E_Step/E_Step_cafet.geojson");
-    const json = JSON.parse(raw)
-    res.send(JSON.stringify(json))
+app.get('/rooms/:name', async (req, res) => {
+    const data = await getRoomGis(req.params.name);
+    res.json(data.rows[0].json_build_object)
 })
 
-app.get('/room', async (req, res) => {
-    const data = await getGis("chillout");
-    let geojson = data.rows[0].json_build_object
-    geojson.properties = {name : "Chillout"}
-    console.log(geojson.properties)
-    res.send(JSON.stringify(geojson))
-})
-*/
 app.listen(3001)
