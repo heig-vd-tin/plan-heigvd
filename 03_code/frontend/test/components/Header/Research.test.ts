31234b22837043e80f5655900e4d67301c626f6c
Klab93 <kylian.bourcoud@gmail.com>
2022-08-03 13:13:53 +0200

    preparation of final version

--- 03_code/frontend/test/components/Header/Research.test.ts
+++ 03_code/frontend/test/components/Header/Research.test.ts
@@ -8,10 +8,3 @@ const wrapper = mount(Research,{})
 test('isComponentVisible', () => {
     expect(wrapper.isVisible())
 })
-
-test('isSuggestionSectionVisible', async () => {
-    await wrapper.find('#research-input').setValue('au')
-    expect(wrapper.findComponent(suggestion).isVisible())
-    await wrapper.find('#research-input').setValue('')
-    expect(wrapper.findComponent(suggestion).isVisible()).toBe(false)
-})
